7133940399a96daf76838c1a49cbdfd509d0cf95
Bousk <bousk@supinfo.com>
2021-01-27 23:15:29 -0500

    initial commit
    + file explorer, folder & file picker
    > can filter files to display only specific extensions
    > can add a callback to return the picked filepath or folder path
    > can assign an icon to file extensions and folder for better display

---
+++ FilePicker.cpp
@@ -0,0 +1,143 @@
+#include <FilePicker.hpp>
+
+#include <imgui/SFML.h>
+
+#include <imgui.h>
+
+#include <sstream>
+
+FilePicker::FilePicker()
+{
+	mPath = std::filesystem::absolute(std::filesystem::current_path());
+}
+void FilePicker::Update()
+{
+	namespace ImGui = ::ImGui;
+
+	if (IsVisible())
+	{
+		if (ImGui::Begin("FilePicker", &mVisible))
+		{
+			// Show the current path with clickable button for each folder to travel through
+			ImGui::Text("Current path : ");
+			std::filesystem::path fullpath;
+			for (const std::filesystem::path& path : mPath)
+			{
+				fullpath /= path;
+				ImGui::SameLine();
+				if (ImGui::Button(path.string().c_str()))
+				{
+					SetPath(fullpath);
+					break;
+				}
+			}
+
+			auto ButtonWithIcon = [](const std::string& name, const sf::Texture& icon)
+			{
+				ImGui::PushID(name.c_str());
+				bool clicked = false;
+				if (icon.getSize().x && icon.getSize().y)
+				{
+					const ImTextureID imTex = SFML::GLHandleToImTexture(icon.getNativeHandle());
+					clicked |= ImGui::ImageButton(imTex, ImVec2(16, 16));
+					ImGui::SameLine();
+				}
+				clicked |= ImGui::Button(name.c_str());
+				ImGui::PopID();
+				return clicked;
+			};
+			auto ButtonFile = [&, this](const std::string& name) { return ButtonWithIcon(name, GetFileIcon(std::filesystem::path(name).extension().string())); };
+			auto ButtonFolder = [&, this](const std::string& name) { return ButtonWithIcon(name, mFolderIcon); };
+			auto IsMatchingFilter = [this](const std::string& extension) { return mSettings.extensionsFilter.empty() || std::any_of(mSettings.extensionsFilter.begin(), mSettings.extensionsFilter.end(), [&](const std::string& ext) { return ext == extension; }); };
+
+			if (mPath.has_parent_path())
+			{
+				const std::filesystem::path parent = mPath.parent_path();
+				if (ButtonFolder(".."))
+				{
+					SetPath(parent);
+				}
+			}
+
+			while (!std::filesystem::exists(mPath))
+				mPath = mPath.parent_path();
+
+			for (const auto& entry : std::filesystem::directory_iterator(mPath))
+			{
+				const std::filesystem::path& path = entry.path();
+				const std::string entryName = std::filesystem::relative(path, mPath).string();
+				if (entry.is_directory())
+				{
+					if (ButtonFolder(entryName))
+					{
+						SetPath(path);
+						break;
+					}
+				}
+				else if (entry.is_regular_file())
+				{
+					if (!mSettings.foldersOnly)
+					{
+						if (IsMatchingFilter(path.filename().extension().string()))
+						{
+							if (ButtonFile(entryName.c_str()))
+							{
+								if (mOnPicked)
+									mOnPicked(path);
+								Hide();
+							}
+						}
+					}
+				}
+				else
+				{
+					ImGui::Text(entryName.c_str());
+				}
+			}
+			if (mSettings.foldersOnly)
+			{
+				if (ImGui::Button("Select this folder"))
+				{
+					if (mOnPicked)
+						mOnPicked(mPath);
+					Hide();
+				}
+			}
+		}
+		ImGui::End();
+	}
+}
+
+void FilePicker::SetPath(const std::filesystem::path& path)
+{
+	mPath = path;
+	// Remove ending / if any
+	if (mPath.wstring().back() == std::filesystem::path::preferred_separator)
+		mPath = mPath.parent_path();
+}
+
+const sf::Texture& FilePicker::GetFileIcon(const std::string& extension) const
+{
+	const auto it = mFileIconPerExtension.find(extension);
+	return it != mFileIconPerExtension.cend() ? it->second : mDefaultFileIcon;
+}
+
+void FilePicker::PickFolder(const std::filesystem::path& startingPath, const OnPicked& onPicked)
+{
+	Settings settings;
+	settings.foldersOnly = true;
+	StartPicking(startingPath, onPicked, settings);
+}
+void FilePicker::PickFile(const std::filesystem::path& startingPath, const OnPicked& onPicked, const std::vector<std::string>& extensionFilters/* = std::vector<std::string>()*/)
+{
+	Settings settings;
+	settings.extensionsFilter = extensionFilters;
+	StartPicking(startingPath, onPicked, settings);
+}
+void FilePicker::StartPicking(const std::filesystem::path& startingPath, const OnPicked& onPicked, const Settings& settings/* = Settings()*/)
+{
+	SetPath(startingPath);
+	Show();
+	mOnPicked = onPicked;
+	mSettings = settings;
+}
---
+++ FilePicker.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <SFML/Graphics/Texture.hpp>
+
+#include <filesystem>
+#include <functional>
+
+class FilePicker : public Singleton<FilePicker>
+{
+public:
+	using OnPicked = std::function<void(const std::filesystem::path&)>;
+	struct Settings {
+		bool foldersOnly{ false };
+		std::vector<std::string> extensionsFilter;
+	};
+public:
+	FilePicker();
+	~FilePicker() = default;
+
+	// Call each frame to refresh and display the explorer
+	void Update();
+
+	// Set a texture to be used as icon near folders
+	void SetFolderIcon(const sf::Texture& icon) { mFolderIcon = icon; }
+	// Set a texture to be used as icon near files with matching extension
+	void SetFileIcon(const std::string& extension, const sf::Texture& icon) { mFileIconPerExtension[extension] = icon; }
+	// Set a texture to be used as icon near file if no specific icon exists for its extension
+	void SetDefaultFileIcon(const sf::Texture& icon) { mDefaultFileIcon = icon; }
+	
+	// Starts picking a folder
+	void PickFolder(const std::filesystem::path& startingPath, const OnPicked& onPicked);
+	// Starts picking a file
+	void PickFile(const std::filesystem::path& startingPath, const OnPicked& onPicked, const std::vector<std::string>& extensionFilters = std::vector<std::string>());
+	void StartPicking(const std::filesystem::path& startingPath, const OnPicked& onPicked, const Settings& settings = Settings());
+
+	inline void Show() { mVisible = true; }
+	inline void Hide() { mVisible = false; }
+	inline bool IsVisible() const { return mVisible; }
+
+private:
+	const sf::Texture& GetFileIcon(const std::string& extension) const;
+	void SetPath(const std::filesystem::path& path);
+
+private:
+	sf::Texture mFolderIcon;
+	sf::Texture mDefaultFileIcon;
+	std::unordered_map<std::string, sf::Texture> mFileIconPerExtension;
+	std::filesystem::path mPath;
+	OnPicked mOnPicked;
+	Settings mSettings;
+	bool mVisible{ false };
+};
